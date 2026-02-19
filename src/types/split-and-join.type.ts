c1a4e8c0ad1b82f8b4a4414268903502b058ac25
Александр Тихоненко <alevettih@ya.ru>
2026-02-19 22:33:16 +0300

    feat(route-template): add support for specifying range in `get` method

--- src/types/split-and-join.type.ts
+++ src/types/split-and-join.type.ts
@@ -2,7 +2,8 @@ import { type Join } from './join.type';
 import { type Slice } from './slice.type';
 import { type Split } from './split.type';
 
-export type SplitAndJoin<S extends string, Index extends number> = Join<
-  Slice<Split<S, '/'>, Index>,
-  '/'
->;
+export type SplitAndJoin<
+  S extends string,
+  From extends number,
+  To extends number,
+> = Join<Slice<Split<S, '/'>, From, To>, '/'>;
